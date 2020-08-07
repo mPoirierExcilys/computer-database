8e20453f866702cba360a879302805d3c4057a8e
mPoirierExcilys <mpoirier@excilys.com>
2020-08-07 16:18:46 +0200

    Swagger-Ui

---
+++ Computer-DataBase/webapprest/src/main/java/com/excilys/cdb/configuration/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.excilys.cdb.configuration;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer{
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("swagger-ui.html")
+	      .addResourceLocations("classpath:/META-INF/resources/");
+	 
+	    registry.addResourceHandler("/webjars/**")
+	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+}
--- Computer-DataBase/webapprest/src/main/java/com/excilys/cdb/configuration/WebConfigRest.java
+++ Computer-DataBase/webapprest/src/main/java/com/excilys/cdb/configuration/WebConfigRest.java
@@ -9,9 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @Configuration
 @ComponentScan(basePackages= {"com.excilys.cdb.controllers"})
 public class WebConfigRest implements WebApplicationInitializer {
@@ -19,7 +17,7 @@ public class WebConfigRest implements WebApplicationInitializer {
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
-		webContext.register(WebConfigRest.class, SpringConfigurationContext.class, ServicesConfiguration.class);
+		webContext.register(WebConfigRest.class, SpringConfigurationContext.class, ServicesConfiguration.class, MvcConfig.class,SwaggerConfig.class);
 		webContext.setServletContext(servletContext);
 		
 		ServletRegistration.Dynamic servlet=servletContext.addServlet("dynamicServlet",new DispatcherServlet(webContext));
